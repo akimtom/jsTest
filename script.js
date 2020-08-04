064ae489e6803b6b8572870354a79641a80140cd
Ivan <example@yandex.ru>
2020-08-04 21:26:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* const qest = confirm('are u here?');
+
+console.log(qest); */
+
+//const answer = prompt("Вам есть 18","18");
+
+//console.log(answer+5);
+
+/* const answers = [];
+answers[0] = prompt('Как ваше имя','');
+answers[1] = prompt('Как ваша фамилия','');
+answers[2] = prompt('Как сколько вам лет',''); 
+ 
+//document.write(answers);
+console.log(typeof(answers));
+console.log(typeof(null));
+ */
+
+ /*const category = "toys";
+
+ console.log(`http://someUrl/${category}/5`);
+
+ const user = "Ivan";
+
+ alert(` Привет ${user}`)*/
+console.log(5+ +'5');
+
+console.log(2*4==8);
